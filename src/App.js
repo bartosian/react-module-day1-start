7533eacf54f41a8ff863763c76287b9c2f713370
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-24 09:48:23 -0400

    Expanded app.js syntax

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+
     render() {
+
+        const formatName = (user) => {
+            return `${user.firstName} ${user.lastName}`;
+        }
+
+        const user = {
+            firstName: 'Harper',
+            lastName: 'Perez',
+            avatarUrl: ''
+        };
+
+        const element = (
+            <h2>
+                Hello, {formatName(user)}!
+            </h2>
+        );
+
         return (
             <div className="App">
                 <h1> Hello Ironhackers! </h1>
+                {element}
             </div>
         );
     }
